3ab72cc3f27692ea971d7292ef7964f8abc0d379
llorenzocardoso <llorenzocardoso1@gmail.com>
2024-08-29 17:03:29 -0300

    arrumando espacamento dos itens na navbar

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="mb-15 flex items-center justify-between py-6 bg-white shadow-md"> {/* Fundo branco com sombra */}
-      <div className="flex flex-shrink-0 items-center">
+    <nav className="mb-15 flex items-center justify-between py-6 bg-white shadow-md"> 
+      <div className="flex flex-shrink-0 items-center ml-8"> {/* Adiciona margem à esquerda */}
         <Link to="/">
-          <img className="mx-2 w-10 rounded-full scale-150 cursor-pointer" src={logo} alt="logo"  />
+          <img className="mx-2 w-10 rounded-full scale-150 cursor-pointer" src={logo} alt="logo" />
         </Link>
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="flex items-center justify-center gap-4 text-2xl mr-8"> {/* Adiciona margem à direita */}
         <Link to="/about" className="text-xl text-gray-800 hover:text-gray-600">About</Link>
         <a href="/path/to/your/resume.pdf" target="_blank" rel="noopener noreferrer" className="text-xl text-gray-800 hover:text-gray-600">
           CV
